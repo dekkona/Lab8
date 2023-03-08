3e1c12ab13de9039a92e82e6f48cbeda64f29f62
Eric Kim <dek@ualberta.ca>
2023-03-07 18:28:56 -0700

    "RED: add test for city count"

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -30,22 +30,22 @@ public class CustomListTest {
 //        assertEquals(true, list.hasCity(city));
 //    }
 //
-    @Test
-    public void testDeleteCity(){
-        CustomList list = new CustomList(null, cityList);
-        City city = new City("Edmonton", "AB");
-        list.addCity(city);
-        list.delete(city);
-        assertEquals(false, list.hasCity(city));
-    }
-//
 //    @Test
-//    public void testCountCities(){
+//    public void testDeleteCity(){
 //        CustomList list = new CustomList(null, cityList);
 //        City city = new City("Edmonton", "AB");
-//        City city2 = new City("Regina", "SK");
 //        list.addCity(city);
-//        list.addCity(city2);
-//        assertEquals(2, list.countCities());
+//        list.delete(city);
+//        assertEquals(false, list.hasCity(city));
 //    }
+//
+    @Test
+    public void testCountCities(){
+        CustomList list = new CustomList(null, cityList);
+        City city = new City("Edmonton", "AB");
+        City city2 = new City("Regina", "SK");
+        list.addCity(city);
+        list.addCity(city2);
+        assertEquals(2, list.countCities());
+    }
 }
